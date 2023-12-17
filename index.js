9633d9bf8413f5e7845db428a53096f389d10b73
Ob55 <brian55mwangi@gmail.com>
2023-12-17 09:30:13 +0300

    done assigment

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+ let cats;
+
+ cats = ["Milo", "Otis", "Garfield"];
+
+ function destructivelyAppendCat(Ralph){
+     cats.push(Ralph)
+   }
+   function destructivelyPrependCat(Bob) {
+     cats.unshift(Bob);
+   }
+    function destructivelyRemoveLastCat(){
+     cats.pop()
+    }
+    function destructivelyRemoveFirstCat(){
+     cats.shift()
+    }
+    let catss = ["Milo","Otis", "Garfield"];
+    function appendCat(Broom){
+     const newCat =[...catss,Broom];
+     return newCat;
+    }
+
+    let brianCats = ["Milo","Otis", "Garfield"];
+     function prependCat(Arnold){
+          const beginningCat =[Arnold , ...brianCats];
+          return beginningCat;
+     }
+     let removesCat =["Milo","Otis", "Garfield"];
+       function removeLastCat(){
+         const removelCats = brianCats.slice(0 , -1)        
+         return removelCats;
+     }      
+     let firstCat =["Milo","Otis", "Garfield"];
+       function removeFirstCat(){
+         const changeCats = firstCat.slice(1)        
+         return changeCats;
+       }
